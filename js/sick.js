b0eda815f8d99a839ffb45b08fd942a834ad1305
Brendan Hanna <gepetto503@gmail.com>
2017-06-30 16:13:32 -0700

    enter a symptom, list of docs displays to user on screen

--- js/sick.js
+++ js/sick.js
@@ -2,19 +2,33 @@ var apiKey = require('./../.env').apiKey;
 
 // object
 Patient = function(){
+}
+
+
+var displayDocs = function(docArray) {
+  $('.results').empty();
+  console.log(docArray);
+  for (var i = 0; i < docArray.length; i++) {
+    $('.results').append(
+    '<div class="doc">' +
+      '<h3>' + docArray[i].profile.first_name + ' ' + docArray[i].profile.last_name + '</h3>' +
+    '</div>');
+  }
 };
+
 
 // prototype
 Patient.prototype.getDocs = function (medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
+   .then(function(response) {
+      displayDocs(response.data);//sending doc array to display docs
+
     })
    .fail(function(error){
       console.log("fail");
+      $('.results').empty().append("<h3>No Docs specialize in your sickness</h3><p><Reflect in darkness./p>")
     });
 };
 
-//api call function goes here
 
 exports.formModule = Patient;
